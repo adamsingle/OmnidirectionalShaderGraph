9a067a43eefcd560688c8f109b8d1f670ef1b7e6
Adam Single <adam.single@unity3d.com>
2020-01-17 21:44:52 +1000

    Initial Commit

---
+++ Assets/Scripts/OmnidirectionalLight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class OmnidirectionalLight : MonoBehaviour
+{
+    /// <summary>
+    /// The parameter name. This must match the one in your shaders
+    /// </summary>
+    private static readonly string _shaderPosName = "_OmniLightPos";
+
+    private static readonly string _shaderColorName = "_OmniLightColor";
+
+    [SerializeField] private Color _lightColor;
+    
+    public void Update()
+    {
+        Shader.SetGlobalVector(_shaderPosName, transform.position);
+        Shader.SetGlobalVector(_shaderColorName, _lightColor);
+    }
+}
---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace OmnidirectionalLightTest
+{
+    /// <summary>
+    /// https://gamedev.stackexchange.com/a/128144
+    /// </summary>
+    public class Orbit : MonoBehaviour
+    {
+        [SerializeField] private float _orbitSpeed = 1f;
+        [SerializeField] private float _alpha = 0f;
+        [SerializeField] private float _tilt = 45f;
+
+        private void Update()
+        {
+            transform.localPosition = new Vector2(
+                0f + (10f * MCos(_alpha) * MCos(_tilt) - 5f * MSin(_alpha) * MSin(_tilt)),
+                0f + (10f * MCos(_alpha) * MSin(_tilt) + 5f * MSin(_alpha) * MCos(_tilt)));
+
+            _alpha += _orbitSpeed;
+        }
+
+        private float MCos(float value)
+        {
+            return Mathf.Cos(Mathf.Deg2Rad * value);
+        }
+
+        private float MSin(float value)
+        {
+            return Mathf.Sin(Mathf.Deg2Rad * value);
+        }
+    }
+}
